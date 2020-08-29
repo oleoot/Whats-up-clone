754b296191f57570c47e0d45db9e79c597c9df92
oleoot <oleoot28@gmail.com>
2020-08-29 18:26:53 +0300

    Info and search in sidebar component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJA1TuX52c0JEPTRGq0ylWplJrUvnPdYg",
+    authDomain: "whats-up-clone-e75a3.firebaseapp.com",
+    databaseURL: "https://whats-up-clone-e75a3.firebaseio.com",
+    projectId: "whats-up-clone-e75a3",
+    storageBucket: "whats-up-clone-e75a3.appspot.com",
+    messagingSenderId: "674065427800",
+    appId: "1:674065427800:web:736811f834c5652976892d"
+};
