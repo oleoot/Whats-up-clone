0651f3053516ccb33a1da8d5e26dee36cb96191c
oleoot <oleoot28@gmail.com>
2020-09-01 12:25:25 +0300

    Sign in with Google functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar/Sidebar';
 import Chat from './components/Sidebar/Chat';
+import Login from './components/Login'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path='/rooms/:roomId'>
-              <Chat />
-            </Route>
+      {!user ? (
+        <Login />
+      ) : (
+          <div className="app__body">
+            <Router>
+              <Sidebar />
+              <Switch>
+                <Route path='/rooms/:roomId'>
+                  <Chat />
+                </Route>
 
-            <Route path='/'>
-              {/* <Chat /> */}
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+                <Route path='/'>
+                </Route>
+              </Switch>
+            </Router>
+          </div>)}
     </div>
   );
 }
